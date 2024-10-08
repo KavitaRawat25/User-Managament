c3292962ea4a2a1113b8fa3cc6e677aabebe4055
Kavita Rawat <kavitarawat0201@gmail.com>
2024-10-08 13:12:38 +0530

    new repo

---
+++ src/main/java/org/jp/UserMgmtApplication.java
@@ -0,0 +1,14 @@
+package org.jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgmtApplication.class, args);
+		System.out.println("Hello!");
+	}
+
+}
