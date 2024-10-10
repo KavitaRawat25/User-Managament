12ce84683ad0469f1c7ff850ba5dbad98d79853b
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-10-10 13:28:42 +0530

    create role api created...

--- src/main/java/org/jp/UserMgmtApplication.java
+++ src/main/java/org/jp/UserMgmtApplication.java
@@ -1,14 +1,23 @@
 package org.jp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "org.jp.repository")
 public class UserMgmtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserMgmtApplication.class, args);
-		System.out.println("Hello!");
+    public static void main(String[] args) {
+        SpringApplication.run(UserMgmtApplication.class, args);
+        System.out.println("started....");
+    }
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
---
+++ src/main/java/org/jp/translater/RoleTranslater.java
@@ -0,0 +1,21 @@
+package org.jp.translater;
+
+import org.jp.dto.UserDtoReq;
+import org.jp.entity.UserRole;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleTranslater{
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public UserRole translateRole(UserDtoReq userDtoReq) {
+		return modelMapper.map(userDtoReq, UserRole.class);
+	}
+	public UserDtoReq translateRoleDto(UserRole userRole) {
+		return modelMapper.map(userRole, UserDtoReq.class);
+	}
+
+}
