f02c18eef66241fe521e0079cca20c20b8b0a75c
Amar66621 <aj4234573@gmail.com>
2024-10-10 14:51:04 +0530

    update created

--- src/main/java/org/jp/dto/UserDtoReq.java
+++ src/main/java/org/jp/dto/UserDtoReq.java
@@ -19,6 +19,8 @@ public class UserDtoReq {
 	private boolean canAccessWebRequest;
 	
 	private int status;
+
+	
 	
 
 }
--- src/main/java/org/jp/translater/RoleTranslater.java
+++ src/main/java/org/jp/translater/RoleTranslater.java
@@ -1,6 +1,7 @@
 package org.jp.translater;
 
 import org.jp.dto.UserDtoReq;
+import org.jp.dto.UserDtoRes;
 import org.jp.entity.UserRole;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,11 @@ public class RoleTranslater{
 	public UserDtoReq translateRoleDto(UserRole userRole) {
 		return modelMapper.map(userRole, UserDtoReq.class);
 	}
+	
+	public UserDtoRes translateRoleToRes(UserRole userRole) {
+		return modelMapper.map(userRole, UserDtoRes.class);
+	}
 
+	
+	
 }
