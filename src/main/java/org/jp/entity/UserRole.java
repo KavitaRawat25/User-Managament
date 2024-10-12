4f24d0e85d2abcea021c7c5a9836ed4e83a1f8e3
Puja-sah-aj <sahpuja452@gmail.com>
2024-10-12 12:05:58 +0545

    User login created

--- src/main/java/org/jp/entity/UserRole.java
+++ src/main/java/org/jp/entity/UserRole.java
@@ -10,8 +10,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import jakarta.persistence.Table;
+import lombok.Data;
 
-
+@Data
 @Entity
 @Table
 public class UserRole {
@@ -38,64 +39,7 @@ public class UserRole {
 	
 	
 	
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	public String getRoleName() {
-		return roleName;
-	}
-
-
-	public void setRoleName(String roleName) {
-		this.roleName = roleName;
-	}
-
-
-	public String getRoleDescription() {
-		return roleDescription;
-	}
-
-
-	public void setRoleDescription(String roleDescription) {
-		this.roleDescription = roleDescription;
-	}
-
-
-	public boolean isCanAccessMobileRequest() {
-		return canAccessMobileRequest;
-	}
-
-
-	public void setCanAccessMobileRequest(boolean canAccessMobileRequest) {
-		this.canAccessMobileRequest = canAccessMobileRequest;
-	}
-
-
-	public boolean isCanAccessWebRequest() {
-		return canAccessWebRequest;
-	}
-
-
-	public void setCanAccessWebRequest(boolean canAccessWebRequest) {
-		this.canAccessWebRequest = canAccessWebRequest;
-	}
-
-
-	public int getStatus() {
-		return status;
-	}
-
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
+	
 
 	
 }
