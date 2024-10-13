152af8178376e8b1b088fd60717443230d864591
Amar66621 <aj4234573@gmail.com>
2024-10-13 11:35:41 +0530

    update changes

--- src/main/java/org/jp/entity/UserEntity.java
+++ src/main/java/org/jp/entity/UserEntity.java
@@ -39,62 +39,6 @@ public class UserEntity {
 
     @Column // Added unique constraint for email
     private String userEmail;
+    private String password;
 
-//    @OneToOne
-//    @JoinColumn(name = "user_role_id") // Set insertable and updatable to false
-//    private UserRole userRole;
-	
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public Long getPhoneNo() {
-		return phoneNo;
-	}
-	public void setPhoneNo(Long phoneNo) {
-		this.phoneNo = phoneNo;
-	}
-	public Long getRoleId() {
-		return roleId;
-	}
-	public void setRoleId(Long roleId) {
-		this.roleId = roleId;
-	}
-	public Boolean getStatus() {
-		return status;
-	}
-	public void setStatus(Boolean status) {
-		this.status = status;
-	}
-	public String getUserEmail() {
-		return userEmail;
-	}
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-	
-//	public UserRole getUserRole() {
-//		return userRole;
-//	}
-//	public void setUserRole(UserRole userRole) {
-//		this.userRole = userRole;
-//	}
-	
-	
 }
